45739c8520d48f3e448c03a376ec3d0220df0cfe
Daniel Goncalves <daniel.goncalves@whatsmoney.com.br>
2021-03-14 18:38:48 -0300

    Add project files.

---
+++ APIObterJuros/Controllers/JurosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIObterJuros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JurosController : ControllerBase
+    {
+        [HttpGet, Route("taxaJuros")]
+        public IActionResult GetTaxaJuros()
+        {
+            return Ok(new { taxa = 0.01 });
+        }
+    }
+}
---
+++ APIObterJuros/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace APIObterJuros
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Obter juros",
+                    Version = "v1",
+                    Contact = new OpenApiContact() { Email = "danielgon131@gmail.com", Name = "Daniel dos Santos Gonçalves", Url = new Uri("https://github.com/danielgonc") }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIObterJuros v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
